917fdb4dfea738549fe10481c079b36b873ae843
Daniel <daniel.molon.santos@escola.pr.gov.br>
2025-09-01 08:22:44 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,15 @@
+console.log("Olá, mundo! Esta é a minha primeira atividade de JavaScript.");
+
+let idade = prompt("Por favor, digite sua idade:");
+
+if (idade >= 18) {
+  console.log("Você é maior de idade.");
+} else {
+  console.log("Você é menor de idade.");
+}
+
+let colegas = ["Ana", "Bruno", "Carla", "Daniel", "Elisa"];
+
+for (let nome of colegas) {
+  console.log(nome);
+}
